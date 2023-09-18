583879c71abb7464c012360b597560456ffb6713
Leonardo Khaoim <LeoKhao2611@gmail.com>
2023-09-18 17:04:59 -0400

    [WIP] - Consagrando y puliendo el retorno de Banesco

--- payment_banesco/utils.py
+++ payment_banesco/utils.py
@@ -1,27 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 def get_API_key(provider_sudo):
-    """ Return the publishable key for Stripe.
-
-    Note: This method serves as a hook for modules that would fully implement Stripe Connect.
-
-    :param recordset provider_sudo: The provider on which the key should be read, as a sudoed
-                                    `payment.provider` record.
-    :return: The publishable key
-    :rtype: str
-    """
     return provider_sudo.banesco_API_key
 
 
 def get_secret_key(provider_sudo):
-    """ Return the secret key for Stripe.
-
-    Note: This method serves as a hook for modules that would fully implement Stripe Connect.
-
-    :param recordset provider_sudo: The provider on which the key should be read, as a sudoed
-                                    `payment.provider` record.
-    :return: The secret key
-    :rtype: str
-    """
     return provider_sudo.banesco_secret_key
 
